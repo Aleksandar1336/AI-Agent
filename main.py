3dc9c670ca0ceb5b33e0637f7e764a2a09929846
Aleksandar1336 <aleksandarhristov943@gmail.com>
2026-02-12 17:38:05 +0200

    updates to AI to show tokens

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import os
+import argparse
 from dotenv import load_dotenv
+from google import genai
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
@@ -10,22 +12,28 @@ if api_key is None:
         "Please set it before running the application."
     )
 
+client = genai.Client(api_key=os.environ["GEMINI_API_KEY"])
 
-from google import genai
-
-client = genai.Client(api_key=api_key)
-
-
-response = client.models.generate_content(
-    model="gemini-2.5-flash",
-    contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-)
-
-print(response.text)
 
 def main():
-    print("Hello from ai-agent!")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    args = parser.parse_args()
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash",
+        contents=args.user_prompt
+    )
+
+    if response.usage_metadata is None:
+        raise RuntimeError("API request failed or returned no usage metadata.")
+
+    print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
+    print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
+
+    print(response.text)
 
 
 if __name__ == "__main__":
     main()
+
